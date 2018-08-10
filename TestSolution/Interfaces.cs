614c386bbfa3234f99969e5098175fc20aec6a1e
Robert Silver <41198337+dev-rsilver@users.noreply.github.com>
2018-08-10 12:00:35 -0400

    Adds OOP and user specified paths.

---
+++ TestSolution/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TestSolution
+{
+    public interface INamesProcessor
+    {
+        
+        /// <summary>
+        /// This method should obtain the name and do something with it, such as adding it to a dictionary.
+        /// </summary>
+        /// <param name="s"></param>
+        string GetName(StreamReader sr);
+    }
+
+    public interface ITextProcessor
+    {
+        /// <summary>
+        /// This 
+        /// </summary>
+        /// <param name="s"></param>
+        string GetText(StreamReader sr);
+    }
+
+}
--- TestSolution/Program.cs
+++ TestSolution/Program.cs
@@ -9,8 +9,22 @@ namespace TestSolution
     {
         static void Main(string[] args)
         {
-            //Initial solution
+            //Allow for user specified file locations.
+            //arg[0]: the path to the names
+            //arg[1]: the path to the list
 
+            if(args.Length <= 0)
+            {
+                args = new string[2];
+                args[0] = "../../../Data/names.txt";
+                args[1] = "../../../Data/list.txt";
+            }
+
+            if (!File.Exists(args[0]))
+                throw new Exception("Invalid names path.");
+
+            if (!File.Exists(args[1]))
+                throw new Exception("Invalid list path.");
 
             //Simply read in names and list and use a dictionary to compare
             //for ~O(1) lookup. Assumes that the names list will fit in memory.
@@ -18,54 +32,22 @@ namespace TestSolution
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            Dictionary<string, List<int>> nameLinePairs = new Dictionary<string, List<int>>();
-
-            using (StreamReader sr = new StreamReader("../../../Data/names.txt"))
-            {
-                var line = "";
-                do
-                {
-                    line = sr.ReadLine();
-
-                    if(line != null)
-                    {
-                        nameLinePairs.Add(line.Trim().ToLower(), new List<int>());
-                    }
-
-
-                } while (line != null);
-
-            }
-
-            using (StreamReader sr = new StreamReader("../../../Data/list.txt"))
-            {
-                int i = 1; //assumes that a 1 based index is desired since we're printing a list
-                var line = "";
-                do
-                {
-                    line = sr.ReadLine();
-
-                    if (line != null)
-                    {
-                        var key = line.Trim().ToLower();
-
-                        if (nameLinePairs.ContainsKey(key))
-                        {
-                            nameLinePairs[key].Add(i);
-                        }
-                        
-                        i++;
-                    }
-
-
-                } while (line != null);
-            }
-
+            INamesProcessor namesProcessor = new Names();
+            ITextProcessor textProcessor = new Text();
+            ComparisonProcessor processor = new ComparisonProcessor(namesProcessor, textProcessor);
+            processor.Compare(args[0], args[1]);
 
             sw.Stop();
 
             System.Diagnostics.Trace.WriteLine("Elapsed time to create dictionary and lookup values:" + sw.ElapsedMilliseconds + "ms");
 
+            Print(processor.NameLinePairs);
+
+            Console.ReadLine();
+        }
+
+        static void Print(Dictionary<string, List<int>> nameLinePairs)
+        {
             //Print the solution
             foreach (string s in nameLinePairs.Keys)
             {
@@ -81,10 +63,6 @@ namespace TestSolution
 
                 Console.WriteLine(s + ":" + list);
             }
-
-            Console.ReadLine();
-
-
         }
     }
 }
